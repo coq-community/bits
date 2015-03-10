ae0e7a60c7d45b79fde0a8758b6e606fa613fde5
Pierre-Évariste Dagand <pierre-evariste.dagand@lip6.fr>
2015-03-10 09:20:17 +0100

    Add repr files

--- src/bits.v
+++ src/bits.v
@@ -1,5 +1,12 @@
+From Coq
+     Require Import ZArith.ZArith.
+
 Require Export Bits.repr.
+Require Export Bits.repr.operations.
+Require Export Bits.repr.properties.
+Require Export Bits.repr.refinement.
 Require Export Bits.spec.
+Require Export Bits.spec.notation.
 Require Export Bits.spec.properties.
 Require Export Bits.spec.operations.
 Require Export Bits.spec.operations.properties.

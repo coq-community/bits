ac99094b91a9593edbd8c1104b615cfdea5c910d
Arthur Blot <arthur.blot78@gmail.com>
2015-09-26 07:46:41 +0200

    Fix test.ml according to our axioms & factorize by wordsize

--- src/extraction/test.ml
+++ src/extraction/test.ml
@@ -1,6 +1,10 @@
-open Axioms32
-
-(* TODO: projected runtime *)
+(*
+ * To try with 8, 16, 32bit, only these two lines need to be replaced.
+ * Note some definitions exported by Coq may need to be hidden.
+ *)
+open Axioms16
+let bits_wordsize = wordsize
+let wordsize = 16
 
 type backtrace 
   = Cause of string
@@ -9,15 +13,14 @@ exception CounterExample of backtrace
 
 let foundCounterExample bt = raise (CounterExample bt)
 
-let wordsize = 8
-let min_int = - (1 lsl (wordsize - 1))
-let max_int = (1 lsl (wordsize - 1) - 1)
+let min_int = 0
+let max_int = (1 lsl wordsize) - 1
+let wordmask = max_int
 
 let print_config () =
   Printf.printf "Wordsize: %d\nMin. int: %d\nMax. int: %d\n\n" wordsize min_int max_int
 
-let step_count = 1 lsl 20
-
+let step_count = 1 lsl (wordsize - 7)
 
 let make_display () =
   let count = ref 0 in
@@ -25,7 +28,7 @@ let make_display () =
       if i mod step_count = 0 then
         begin
           incr count;
-(*          Printf.printf "%d/1024\n%!" !count; *)
+          (*Printf.printf "%d/128\n" !count;*)
         end)
   
 let assign (v: string)(a: 'a)(p: 'a -> string)(k : 'a -> unit): unit =
@@ -64,18 +67,21 @@ let equal pp ls l r rs =
   if l <> r then
     foundCounterExample (Cause (Printf.sprintf "%s = %s != %s = %s" ls (pp l) (pp r) rs))
 
-let fromInt32_cancel () =
-  Printf.printf "forall i : Int32, toInt32 (fromInt32 i) = i";
-  let progress = make_display () in
-  forall "i" (fun i ->
-    equal string_of_int "toInt32 (fromInt32 i)" (toInt32 (fromInt32 i)) i "i");
-  Printf.printf "\nSuccess!\n"
-
-let toInt32_cancel () =
-  Printf.printf "forall bs : BITS 32, fromInt32 (toInt32 bs) = bs";
+let bitsToIntK_test () =
+  Printf.printf "bitsToIntK_test";
   forall_bits "bs" (fun bs -> 
-    equal ppbits "fromInt32 (toInt32 bs)" (fromInt32 (toInt32 bs)) bs "bs");
-  Printf.printf "\nSuccess!\n"
+    equal ppbits "bitsFromInt (bitsToInt bs)" (bitsFromInt (bitsToInt bs)) bs "bs");
+  Printf.printf "\nSuccess!\n"; flush stdout
+
+let bitsFromInt_inj () =
+  Printf.printf "bitsFromInt_inj";
+  forall "i" (fun i ->
+    forall "i'" (fun i' ->
+      let bs = bitsFromInt i in
+      let bs' = bitsFromInt i' in
+      if (bs = bs' && i <> i') then
+        foundCounterExample (Cause (Printf.sprintf "bs = bs' -> i = %s != %s = i'" (string_of_int i) (string_of_int i')))));
+  Printf.printf "\nSuccess!\n"; flush stdout
 
 let native_reprP ls l r rs =
   if not (native_repr l r) then
@@ -83,104 +89,79 @@ let native_reprP ls l r rs =
 
 let zero_repr () =
   Printf.printf "native_repr 0 #0";
-  native_reprP "0" 0 (fromNat Axioms32.wordsize O) "#0";
-  Printf.printf "\nSuccess!\n"
+  native_reprP "0" 0 (fromNat bits_wordsize O) "#0";
+  Printf.printf "\nSuccess!\n"; flush stdout
 
 let one_repr () =
   Printf.printf "native_repr 1 #1";
-  native_reprP "1" 1 (fromNat Axioms32.wordsize (S O)) "#1";
-  Printf.printf "\nSuccess!\n"
+  native_reprP "1" 1 (fromNat bits_wordsize (S O)) "#1";
+  Printf.printf "\nSuccess!\n"; flush stdout
 
-let succ i = if i = 127 then -128 else i + 1
+let succ = fun x -> (x + 1) land wordmask
 
-let succ_repr () =
-  Printf.printf "forall (i: Int8)(bs: BITS 8), native_repr i bs -> native_repr (succ i) (incB bs)";
-  forall "i" (fun i ->
-    let bs = fromInt32 i in
-    native_reprP "succ i" (succ i)
-                 (incB Axioms32.wordsize bs) "incB (fromInt32 i)");
-  Printf.printf "\nSuccess!\n"
+let neg = fun x -> (-x) land wordmask
 
-let comp_repr () =
-  Printf.printf "forall (i i': Int8)(bs bs': BITS 8), native_repr i bs -> native_repr i' bs' -> (i = i') = (bs = bs')";
-  forall "i" (fun i ->
-    forall "i'" (fun i' ->
-      List.iter (fun (lname, lcomp, compB, nameB) ->
-        let bs = fromInt32 i in
-        let bs' = fromInt32 i' in
-        equal string_of_bool
-          ("(i " ^ lname ^ " i')") (lcomp i i') 
-          (compB bs bs') ("(fromInt32 i " ^ nameB ^ " fromInt32 i')"))
-      [("<", (<), ltB Axioms32.wordsize, "ltB");
-       ("=", (=), (=), "==")]));
-  Printf.printf "\nSuccess!\n"
+let not_int = fun x -> (lnot x) land wordmask
 
-let neg x = if x = -128 then -128 else -x
-
-let dec x = if x = -128 then 127 else x-1
+let dec = fun x -> (x - 1) land wordmask
 
 let unop_repr () =
   Printf.printf "forall (i: Int8)(bs: BITS 8), native_repr i bs -> forall lop opB, native_repr (lop i) (opB bs)";
   forall "i" (fun i ->
-    let bs = fromInt32 i in
+    let bs = bitsFromInt i in
     List.iter (fun (lname, lop, opB, name) ->
       native_reprP (lname ^ " i") (lop i) 
-        (opB bs) (name ^ "(fromInt32 i)"))
-      [("not", lnot, invB Axioms32.wordsize, "invB");
-       ("neg", neg, negB Axioms32.wordsize, "negB");
-       ("dec", dec, decB Axioms32.wordsize, "decB")]);
-  Printf.printf "\nSuccess!\n"
+        (opB bs) (name ^ "(bitsFromInt i)"))
+      [("succ", succ, incB bits_wordsize, "incB");
+       ("not", not_int, invB bits_wordsize, "invB");
+       ("neg", neg, negB bits_wordsize, "negB");
+       ("dec", dec, decB bits_wordsize, "decB")]);
+  Printf.printf "\nSuccess!\n"; flush stdout
 
-let add_int8 x y = 
-  let s = x + y in
-  if s < -128 then 
-    s + 256
-  else if s > 127 then
-    s - 256
-  else 
-    s
+let add_int = fun x y -> (x + y) land wordmask
 
 let binop_repr () =
   Printf.printf "forall (i i': Int8)(bs bs': BITS 8), native_repr i bs -> native_repr i' bs' -> forall lop opB, native_repr (lop i i') (opB bs bs')";
   forall "i" (fun i ->
     forall "i'" (fun i' ->
-      let bs = fromInt32 i in
-      let bs' = fromInt32 i' in
+      let bs = bitsFromInt i in
+      let bs' = bitsFromInt i' in
       List.iter (fun (lname, lop, opB, name) -> 
         native_reprP (lname ^ " i i'") (lop i i')
                      (opB bs bs') (name ^ " i i'"))
-        [("(land)", (land), andB Axioms32.wordsize, "andB");
-         ("(lor)", (lor), orB  Axioms32.wordsize, "orB");
-         ("(lxor)", (lxor), xorB Axioms32.wordsize, "xorB");
-         ("(+)", add_int8, (fun x y -> snd (adcB Axioms32.wordsize false x y)), "addB");
+        [("(land)", (land), andB bits_wordsize, "andB");
+         ("(lor)", (lor), orB  bits_wordsize, "orB");
+         ("(lxor)", (lxor), xorB bits_wordsize, "xorB");
+         ("(+)", add_int, (fun x y -> snd (adcB bits_wordsize false x y)), "addB");
         ]));
-  Printf.printf "\nSuccess!\n"
+  Printf.printf "\nSuccess!\n"; flush stdout
 
-let forall_nat32 var k =
+let forall_nat var k =
   let rec help acc = function
     | 0 -> 
-       assign var (acc O) (fun _ -> "32") k
+       assign var (acc O) (fun _ -> string_of_int wordsize) k
     | x ->
        begin
-         assign var (acc O) (fun _ -> string_of_int (32 - x)) k;
+         assign var (acc O) (fun _ -> string_of_int (wordsize - x)) k;
          help (fun x -> S (acc x)) (x - 1)
        end
   in
-  help (fun x -> x) 32
-       
+  help (fun x -> x) wordsize
+
+let lsl_int = fun x y -> (x lsl y) land wordmask
 
 let shift_repr () =
   Printf.printf "forall (i: Int8)(bs: BITS 8)(n: nat), native_repr i bs -> forall lshift shiftB, native_repr (lshift l #n) (shiftB l n)";
   forall "i" (fun i ->
-    forall_nat32 "n" (fun n ->
-      let bs = fromInt32 i in
-      let nInt = toInt32 (fromNat Axioms32.wordsize n) in
+    forall_nat "n" (fun n ->
+      let bs = bitsFromInt i in
+      let nInt = bitsToInt (fromNat bits_wordsize n) in
       List.iter (fun (lname, lop, opB, nameB) ->
         native_reprP (lname ^ " i n") (lop i nInt)
                     (opB bs n) (nameB ^ " i n"))
-        [("(lsr)", (lsr), shrBn Axioms32.wordsize, "shrBn");
-         ("(lsl)", (lsl), shlBn Axioms32.wordsize, "shlBn")]));
-  Printf.printf "\nSuccess!\n"
+        [("(lsr)", (lsr), shrBn bits_wordsize, "shrBn");
+         ("(lsl)", lsl_int, shlBn bits_wordsize, "shlBn")]));
+  Printf.printf "\nSuccess!\n"; flush stdout
 
 let rec pprint_bt = function
   | Cause s -> Printf.printf "%s\n" s
@@ -190,19 +171,16 @@ let rec pprint_bt = function
        Printf.printf "with %s = %s\n" x v;
      end
 
-
-
 let _ =
   print_config();
   try
-    fromInt32_cancel ();
-    toInt32_cancel ();
+    bitsToIntK_test ();
+    bitsFromInt_inj ();
     zero_repr ();
     one_repr ();
-    succ_repr ();
-    leq_repr ();
     unop_repr ();
     binop_repr ();
+    shift_repr ()
   with
   | CounterExample bt -> pprint_bt bt
   

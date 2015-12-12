64e40d12da0da7c3ea188e5212a6d7d70f322069
Arthur Blot <arthur.blot78@gmail.com>
2015-12-12 19:39:25 +0900

    Remove test.ml & update verif.sh

--- src/extraction/verif.sh
+++ src/extraction/verif.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-# Remove interface file, as it causes some issues
-rm -f axioms8.mli
 # Some handmade patching needed for unknown reasons
 patch axioms8.ml magic.patch
+patch axioms16.ml magic.patch
 
 echo "Verifying 8bit"
 ocamlbuild axioms8.native && ./axioms8.native
 echo "... Ok"
 echo "Verifying 16bit"
-ocamlbuild test.native && ./test.native
+ocamlbuild axioms16.native && ./axioms16.native
 echo "... Ok"
 cd -
